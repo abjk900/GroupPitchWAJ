e1f666f0222bac9a6f62881bbbdb799bf37fdf83
Tan Wei Liang <weiliang98321@gmail.com>
2017-09-26 12:28:57 +0800

    done FB login and sign up for the new user

--- g-erms/SignUpViewController.swift
+++ g-erms/SignUpViewController.swift
@@ -8,9 +8,29 @@
 
 import UIKit
 import CountryPicker
+import FirebaseDatabase
+import FirebaseAuth
+import FirebaseStorage
+import FBSDKLoginKit
 
 class SignUpViewController: UIViewController, CountryPickerDelegate {
     @IBOutlet weak var picker: CountryPicker!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var firstnameTextField: UITextField!
+    @IBOutlet weak var lastnameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    @IBOutlet weak var signupButtonTapped: UIButton!{
+        
+        didSet{
+            
+            signupButtonTapped.addTarget(self, action: #selector(signUp), for: .touchUpInside)
+        }
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +42,58 @@ class SignUpViewController: UIViewController, CountryPickerDelegate {
         picker.showPhoneNumbers = false
         picker.setCountry(code!)
     }
-
+    
     // a picker item was selected
     func countryPhoneCodePicker(_ picker: CountryPicker, didSelectCountryWithName name: String, countryCode: String, phoneCode: String, flag: UIImage) {
         //pick up anythink
         //code.text = phoneCode
     }
-   
+    
+    @objc func signUp() {
+        
+        
+        guard let email = emailTextField.text,
+            let password = passwordTextField.text,
+            let confirmPassword = confirmPasswordTextField.text,
+            let userName = usernameTextField.text,
+            let firstName = firstnameTextField.text,
+            let lastName = lastnameTextField.text
+           // let countryPicker = picker.textInputContextIdentifier
+            
+            else {return}
+        
+        
+        if password != confirmPassword {
+            createErrorVC("Password Error", "Password does not match")
+        } else if email == "" || password == "" || confirmPassword == "" {
+            createErrorVC("Missing input fill", "Please fill up your info appropriately in the respective spaces.")
+        }
+        
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                self.createErrorVC("Error", error.localizedDescription)
+            }
+            
+            if let validUser = user {
+                let ref = Database.database().reference()
+                
+                // let post : [String:Any] = ["email": email, "name": userName]
+                let post : [String:Any] = ["name": userName, "email": email, "firstName": firstName ,"lastName": lastName, "imageURL": "","imageFilename": ""]
+                
+                ref.child("Users").child(validUser.uid).setValue(post)
+                
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+    
+    func createErrorVC(_ title: String, _ message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
