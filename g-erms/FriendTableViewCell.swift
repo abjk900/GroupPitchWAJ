371a09bb6859889f46c5d24843d7d1dc39afc727
Audrey Lim <audim98@icloud.com>
2017-09-26 14:18:34 +0800

    Created all ViewController Files and TableViewCell Swift files

---
+++ g-erms/FriendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendTableViewCell.swift
+//  g-erms
+//
+//  Created by Audrey Lim on 26/09/2017.
+//  Copyright © 2017 Audrey Lim. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
