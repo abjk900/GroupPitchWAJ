18be398b0924582961975cbd96b78c08f07ac16e
Audrey Lim <audim98@icloud.com>
2017-09-27 18:22:49 +0800

    Created IBOUtlet for EventVC

--- g-erms/EventTournament/EventTableViewCell.swift
+++ g-erms/EventTournament/EventTableViewCell.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class EventTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var gameLogo: UIImageView!
+    @IBOutlet weak var gameNameLabel: UILabel!
+    @IBOutlet weak var gameDateLabel: UILabel!
+    @IBOutlet weak var gameEventNameLabel: UILabel!
+    @IBOutlet weak var countryPlayerLabel: UILabel!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBAction func buttonFollowingTapped(_ sender: Any) {
+        
+    }
+    
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
