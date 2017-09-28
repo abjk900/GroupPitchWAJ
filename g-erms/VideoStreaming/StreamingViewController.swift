96e3107a2f59a04c50c9ffaaf7172d98ef97990f
Jae Kee Li <abjk900@gmail.com>
2017-09-28 09:57:55 +0800

    making code about attaching video code.

--- g-erms/VideoStreaming/StreamingViewController.swift
+++ g-erms/VideoStreaming/StreamingViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+//matching with url dictionary
+struct SwitchVideo : Decodable {
+    let id : String
+    let name : String
+    let link : String
+    let imageUrl : String
+}
+
 class StreamingViewController: UIViewController {
     
     //****** All the object library *******
@@ -21,7 +29,35 @@ class StreamingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
+    }
+    
+    func feachVideo() {
+        //using with url
+        let jsonUrlString = "http://api.letsbuildthatapp.com/jsondecondable/course"
+        
+        guard let url = URL(string : jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("DataTask Error : \(err.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("Invaild Data")
+                return
+            }
+            
+            //do could be using instead of guald let
+            do{
+                //the "SwitchVideo" need to be chnaged if it has covering array or dictionary.
+                let videoStreaming = try JSONDecoder().decode(SwitchVideo.self, from: data)
+            } catch let jsonErr {
+                print("Error serializing json:", jsonErr)
+            }
+        }
+        
     }
 
    
