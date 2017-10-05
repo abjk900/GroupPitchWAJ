6c3f81d90e5d5a674b10307fc8f09e09bca57e28
Tan Wei Liang <weiliang98321@gmail.com>
2017-10-05 18:02:49 +0800

    leaderboard 90% done

--- g-erms/LeaderboardViewController.swift
+++ g-erms/LeaderboardViewController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseStorage
+import FirebaseAuth
 
 class LeaderboardViewController: UIViewController {
+    
+    var selectedGamer : Gamer?
+    var ref : DatabaseReference!
+    var userId : String = ""
+    var gamers : [Gamer] = []
     
     //****** All the object library *******
     @IBOutlet weak var segmentedController: UISegmentedControl!
@@ -19,9 +27,88 @@ class LeaderboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fetchGamers()
+        
+        leaderboardTableView.dataSource = self
+        leaderboardTableView.delegate = self
 
         // Do any additional setup after loading the view.
     }
 
+    func fetchGamers () {
+        
+        ref = Database.database().reference()
+        guard let uid = Auth.auth().currentUser?.uid else {return}
+        userId = uid
+        
+        ref.child("Top10Gamers").observe(.childAdded, with: { (snapshot) in
+            
+            guard let info = snapshot.value as? [String : Any] else {return}
+            
+            if  let name = info["name"] as? String,
+                let earnings = info["earnings"] as? String,
+                let game = info["game"] as? String,
+                let playerID = info["playerID"] as? String,
+                let tournament = info["tournamnet"] as? String,
+                let profileImageURL = info["profileImageURL"] as? String
+         {
+                
+                let newGamers = Gamer(aName: name, aGameTypes: game, aTournament: tournament, anEarnings: earnings, aPlayerID: playerID, aProfileImage: profileImageURL)
+                
+                self.selectedGamer = newGamers
+                
+                print(newGamers)
+                
+            
+                
+                
+                
+                // self.contacts.removeAll()
+                self.gamers.append(newGamers)
+                
+                return
+            }
+        })
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 
 }
+
+extension LeaderboardViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gamers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "leaderboardCell") else {return UITableViewCell() }
+        
+        let gamer = gamers[indexPath.row]
+        
+        cell.textLabel?.text = gamer.name
+        cell.detailTextLabel?.text = gamer.earnings
+        
+        return cell
+    }
+}
+
+extension LeaderboardViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let destination = storyboard?.instantiateViewController(withIdentifier: "Top10DetailsViewController") as? Top10DetailsViewController else {return}
+        
+        let selectedGamer = gamers[indexPath.row]
+        
+        destination.selectedGamer = selectedGamer
+        
+        navigationController?.pushViewController(destination, animated: true)
+    }
+}
+
---
+++ g-erms/Model/Gamer.swift
@@ -0,0 +1,40 @@
+//
+//  Gamer.swift
+//  g-erms
+//
+//  Created by Tan Wei Liang on 05/10/2017.
+//  Copyright © 2017 Audrey Lim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Gamer {
+    var name : String = ""
+    var gameTypes : String = ""
+    var tournament : String = ""
+    var earnings : String = ""
+    var playerID : String = ""
+    var profileImage : String = ""
+    
+    
+    init (aName : String , aGameTypes : String , aTournament : String , anEarnings : String , aPlayerID : String , aProfileImage : String) {
+        name = aName
+        gameTypes = aGameTypes
+        tournament = aTournament
+        earnings = anEarnings
+        playerID = aPlayerID
+        profileImage = aProfileImage
+        
+      
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
