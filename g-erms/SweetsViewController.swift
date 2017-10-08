e3fdc17e98f572f64c01677a6c19c6025fce6f19
Tan Wei Liang <weiliang98321@gmail.com>
2017-10-08 15:32:01 +0800

    minor fixes

--- g-erms/SweetsViewController.swift
+++ g-erms/SweetsViewController.swift
@@ -13,7 +13,7 @@ class SweetsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
 
    
