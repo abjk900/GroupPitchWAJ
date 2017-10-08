3b5a4ecebbcc3ccfb821a518cb00dc85d40ebcd5
Audrey Lim <audim98@icloud.com>
2017-10-08 10:50:09 +0800

    fixing in prgress the date issue

--- g-erms/EventTournament/EventViewController.swift
+++ g-erms/EventTournament/EventViewController.swift
@@ -9,22 +9,19 @@
 import UIKit
 import FirebaseDatabase
 import FirebaseStorage
-import FirebaseAuth
-import SDWebImage
-
 
 class EventViewController: UIViewController {
 
-    //****** All the object library *******
-    var ref : DatabaseReference!
-    var events : [Event] = []
     let currDate : Date = Date()
-    
+    var tmpDate : Double = 0.0
+    var dummDate : String = ""
+    var dataFilter = 0
+   
     var filteredEvents : [Event] = []
     
-//    var pastEvents : [Event] = []
-//    var ongoingEvents : [Event] = []
-//    var comingEvents : [Event] = []
+    var pastEvents : [Event] = []
+    var ongoingEvents : [Event] = []
+    var comingEvents : [Event] = []
     
     
     @IBOutlet weak var eventViewTableView: UITableView! {
@@ -40,6 +37,10 @@ class EventViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tmpDate = currDate.timeIntervalSince1970
+        dummDate = DateHelper.createDateString(tmpDate)
+        
         eventViewTableView.delegate = self
         eventViewTableView.dataSource = self
         
@@ -54,37 +55,52 @@ class EventViewController: UIViewController {
     
     @IBAction func segmentedControl(_ sender: Any) {
         filteredEvents = []
-        let tmpDate = currDate.timeIntervalSince1970
-        let dummDate = DateHelper.createDateString(tmpDate)
         
-        if selectionSegmentControl.selectedSegmentIndex == 0 {  //pass event
+//        let tmpDate = currDate.timeIntervalSince1970
+//        let dummDate = DateHelper.createDateString(tmpDate)
+        switch selectionSegmentControl.selectedSegmentIndex {
+        case 0:
+            dataFilter = 0
+        case 1:
+            dataFilter = 1
+        case 2:
+            dataFilter = 2
+        default:
+            print("Do Nothing Sesgment Control default")
+        }
+        reload()
+
         
-            for (index , each) in events.enumerated() {
-                if each.eventDate < dummDate {
-                    filteredEvents[index] = events[index]
-                }
-            }
-            
-        } //0
+//
+//        if selectionSegmentControl.selectedSegmentIndex == 0 {  //pass event
+//
+//            for (index , each) in events.enumerated() {
+//                if each.eventDate < dummDate {
+//                    filteredEvents[index] = events[index]
+//                }
+//            }
+//
+//        } //0
+//
+//        if selectionSegmentControl.selectedSegmentIndex == 1 {  //ongoing = current
+//
+//            for (index , each) in events.enumerated() {
+//                if each.eventDate == dummDate {
+//                    filteredEvents[index] = events[index]
+//                }
+//            }
+//
+//        }  //1
+//
+//        if selectionSegmentControl.selectedSegmentIndex == 2 {  //coming events
+//            for (index , each) in events.enumerated() {
+//                if each.eventDate > dummDate {
+//                    filteredEvents[index] = events[index]
+//                }
+//            }
+//
+//        } //2
         
-        if selectionSegmentControl.selectedSegmentIndex == 1 {  //ongoing = current
-            for (index , each) in events.enumerated() {
-                if each.eventDate == dummDate {
-                    filteredEvents[index] = events[index]
-                }
-            }
-            
-        }  //1
-        
-        if selectionSegmentControl.selectedSegmentIndex == 2 {  //coming events
-            for (index , each) in events.enumerated() {
-                if each.eventDate > dummDate {
-                    filteredEvents[index] = events[index]
-                }
-            }
-            
-        } //2
-           
            
          //end selection Segment
 
@@ -93,9 +109,7 @@ class EventViewController: UIViewController {
     
     func fetchEvents() {
         ref = Database.database().reference()
-        
-        
-        
+  
         
         //observer child added works as a loop return each child individually
     //    ref.child("Users").queryOrdered(byChild: "eventDate").queryEqual(toValue: currDate).observe(.childAdded, with: { (snapshot) in
@@ -135,7 +149,21 @@ class EventViewController: UIViewController {
                 let newEvent = Event(anEventId: snapshot.key, aGameName: gameName, anEventName: eventName, anEventDate: self.createDateString(eventDate), anEventTime: self.createTimeString(eventDate), anImageURL: imageURL, aFilename: filename, aPlayer1Name: player1Name, aPlayer2Name: player2Name, aPlayer1Country: player1Country, aPlayer2Country: player2Country, aplayer1FlagImage: player1Flag, aplayer2FlagImage: player2Flag)
                 
                 //append to event array
+                //dummDate
                 self.events.append(newEvent)
+                
+                let cvtDate = DateHelper.createDateString(eventDate)
+                
+                switch cvtDate {
+                case _ where cvtDate < self.dummDate:
+                    self.pastEvents.append(newEvent)
+                case _ where cvtDate == self.dummDate:
+                    self.ongoingEvents.append(newEvent)
+                case _ where cvtDate > self.dummDate:
+                    self.comingEvents.append(newEvent)
+                default :
+                    self.comingEvents.append(newEvent)
+                }
   
                 //this is more efficient
                 //insert indv rows as we retrive idv items
@@ -198,6 +226,13 @@ class EventViewController: UIViewController {
         
     } // fetchEvents
     
+    func reload() {
+        DispatchQueue.main.async {
+            self.eventViewTableView.reloadData()
+        }
+    }
+   
+    
     func createDateString(_ timeStamp: Double) -> String {
         let date = Date(timeIntervalSince1970: timeStamp)
         
@@ -223,12 +258,14 @@ class EventViewController: UIViewController {
 extension EventViewController : UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if filteredEvents.count == 0 {
+        switch dataFilter {
+        case 0: return pastEvents.count
+        case 1: return ongoingEvents.count
+        case 2 : return comingEvents.count
+        default:
             return events.count
-        } else {
-            return filteredEvents.count
         }
-        //return filteredEvents.count  //events.count
+        //return filteredEvents.count  //events.count    //filteredEvents.count
     }
     
     
@@ -237,73 +274,48 @@ extension EventViewController : UITableViewDataSource {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: EventViewCell.cellIdentifier) as? EventViewCell else {return UITableViewCell()}
         
+        var rec = events[indexPath.row]
         
-        if filteredEvents.count == 0 {
-            let rec = events[indexPath.row]
-            
-            cell.gameNameLabel.text = rec.eventGameName
-            cell.gameEventNameLabel.text = rec.eventName
-            cell.gameDateLabel.text = rec.eventDate
-            let imageURL = rec.imageURL
-            cell.gameLogo.loadImage(from: imageURL)
-            
-            // create an NSMutableAttributedString that we'll append everything to
-            let fullString = NSMutableAttributedString(string: "")
-            
-            // create our NSTextAttachment
-            let image1Attachment = NSTextAttachment()
-            image1Attachment.image = UIImage(named: rec.player1FlagImage.lowercased())
-            let image2Attachment = NSTextAttachment()
-            image2Attachment.image = UIImage(named: rec.player2FlagImage.lowercased())
-            
-            // wrap the attachment in its own attributed string so we can append it
-            let image1String = NSAttributedString(attachment: image1Attachment)
-            let image2String = NSAttributedString(attachment: image2Attachment)
-            
-            // add the NSTextAttachment wrapper to our full string, then add some more text.
-            fullString.append(image1String)
-            fullString.append(NSAttributedString(string: " \(rec.player1Name) vs "))
-            fullString.append(image2String)
-            fullString.append(NSAttributedString(string: " \(rec.player2Name)"))
-            
-            // draw the result in a label
-            cell.countryPlayerLabel.attributedText = fullString
+        switch dataFilter {
+        case 0:
+            rec = pastEvents[indexPath.row]
+        case 1:
+            rec = ongoingEvents[indexPath.row]
+        case 2:
+           rec = comingEvents[indexPath.row]
+        default :
+            rec = events[indexPath.row]
         }
-        
-        else {
+
             
-        let rec = filteredEvents[indexPath.row]   //let rec = events[indexPath.row]
-        
         cell.gameNameLabel.text = rec.eventGameName
         cell.gameEventNameLabel.text = rec.eventName
         cell.gameDateLabel.text = rec.eventDate
         let imageURL = rec.imageURL
         cell.gameLogo.loadImage(from: imageURL)
-       
+        
         // create an NSMutableAttributedString that we'll append everything to
         let fullString = NSMutableAttributedString(string: "")
-
+        
         // create our NSTextAttachment
         let image1Attachment = NSTextAttachment()
         image1Attachment.image = UIImage(named: rec.player1FlagImage.lowercased())
         let image2Attachment = NSTextAttachment()
         image2Attachment.image = UIImage(named: rec.player2FlagImage.lowercased())
-
+        
         // wrap the attachment in its own attributed string so we can append it
         let image1String = NSAttributedString(attachment: image1Attachment)
         let image2String = NSAttributedString(attachment: image2Attachment)
-
+        
         // add the NSTextAttachment wrapper to our full string, then add some more text.
         fullString.append(image1String)
         fullString.append(NSAttributedString(string: " \(rec.player1Name) vs "))
         fullString.append(image2String)
         fullString.append(NSAttributedString(string: " \(rec.player2Name)"))
-
+        
         // draw the result in a label
         cell.countryPlayerLabel.attributedText = fullString
    
-        }
-        
         
         return cell
     }
