7c2d7569ba1bc3d791761fed7dba246e6383af3c
Audrey Lim <audim98@icloud.com>
2017-10-10 11:19:56 +0800

    updated the streamVC

--- g-erms/VideoStreaming/StreamingController.swift
+++ g-erms/VideoStreaming/StreamingController.swift
@@ -167,60 +167,6 @@ class StreamingController: UIViewController, UISearchBarDelegate {
     
 } //end streaming controller
 
-//        }
-//            dictionaries.forEach({ (key, value) in
-//                guard let dictionary = value as? [String : Any] else { return }
-//                let videoInfo = VideoInfo(dictionary : dictionary)
-//                print(videoInfo.videoName)
-//                print(videoInfo.videoDescription)
-//                print(videoInfo.videoUrlName)
-//
-//                let newVideoInfo = VideoInfo(dictionary: dictionary)
-//
-//                self.videoInfo.append(newVideoInfo)
-//
-//                let index = self.videoPosts.count - 1
-//                let indexPath = IndexPath(row: index, section: 0)
-//                self.streamingTableView.insertRows(at: [indexPath], with: .right)
-//
-//
-//            })
-//            DispatchQueue.main.async {
-//                self.streamingTableView.reloadData()
-//            }
-//        }
-//
-//    )}
-//}
-        //look the data that in the database and snapshot these values
-//        let ref = Database.database().reference().child("PostVideo").child(uid)
-//        ref.observeSingleEvent(of: .value) { (snapshot) in
-//            //for saving whole value that in each uid(user)
-//            guard let dictionaries = snapshot.value as? [String:Any] else {return}
-//
-//            dictionaries.forEach({ (key, value) in
-//                //for saving each value in uid
-//                //dictionaries 에 모든 값을 저장시키고 그 값을 dictionary로 분류시키는데 여기서 데이터베이스에 있는 정보를 videoInfo에 넣는다.
-//                guard let dictionary = value as? [String:Any] else { return }
-//
-//                let videoInfo = VideoInfo(dictionary: dictionary)
-//                print(videoInfo.videoName)
-//                print(videoInfo.videoDescription)
-//                print(videoInfo.videoUrlName)
-//                //append to event array
-//                self.videoPosts.append(videoInfo)
-//
-//            })
-//
-//            DispatchQueue.main.async {
-//                self.streamingTableView?.reloadData()
-//            }
-//        }
-        
-    
-    
-
-
 
 extension StreamingController : UITableViewDataSource {
     
@@ -236,8 +182,8 @@ extension StreamingController : UITableViewDataSource {
         guard let cell = streamingTableView.dequeueReusableCell(withIdentifier: "playingVideoCell", for: indexPath) as? StreamingTableViewCell  else { return UITableViewCell() }
         
         //2. setup
-        cell.delegate = self as StreamingTableViewCellDelegate
-
+        cell.delegate = self
+        
         if searchActive {
              let videoInfo = filtered[indexPath.row]
        
